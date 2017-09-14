af978b52f0f9a6be7a3ce77c9340bd4cd1d3a0b8
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-09-14 20:58:16 +0200

    More tests for FileSystemService

--- test/service.spec.ts
+++ test/service.spec.ts
@@ -2,12 +2,20 @@ import {FileSystemService} from '../src/service';
 import {expect} from 'chai';
 import 'mocha';
 import * as mockfs from 'mock-fs';
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
 
 describe('FileSystemService', () => {
   let fs = new FileSystemService();
   mockfs({
     content: {
-      file1: 'file1 contents'
+      file1: 'file1 contents',
+      subdir: {
+        file2: 'file2 contents',
+        file3: 'file3 contents'
+      }
     }
   })
 
@@ -15,9 +23,31 @@ describe('FileSystemService', () => {
     mockfs.restore();
   });
 
-  it('should read a file', () => {
-    return fs.readFile('file1').then(data => {
-      expect(data).to.eql('file1 contents');
+  describe('readFile', () => {
+    it('should read a file', () => {
+      return fs.readFile('file1').then(data => {
+        expect(data).to.eql('file1 contents');
+      });
+    });
+
+    it('should read fail to read a file that does not exist', () => {
+      return expect(fs.readFile('file2')).to.be.rejected;
+    });
+  });
+
+  describe('readDir', () => {
+    it('should read a directory', () => {
+      return fs.readDir('subdir').then(files => {
+        expect(files).to.eql(['file2', 'file3']);
+      });
+    });
+
+    it('should fail to read a directory that does not exist', () => {
+      return expect(fs.readDir('noSuchDir')).to.be.rejected;
+    });
+
+    it('should fail to read a path that is a file', () => {
+      return expect(fs.readDir('file1')).to.be.rejected;
     });
   });
 });
