a51e15736e8900bbba412f96ed9fcdd73fa790d7
Björn Andersson <bjorn.olof.andersson@gmail.com>
2017-09-16 10:07:18 +0200

    Added dependency on IsimudPersistence in component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {directory} from './sources/directory';
 export * from './interfaces';
 
 import {component} from '@ziggurat/tiamat';
+import {IsimudPersistence} from '@ziggurat/isimud-persistence';
 import {FileSystemService} from './service';
 import {FileSystemReporter} from './reporter';
 
@@ -10,6 +11,9 @@ import {FileSystemReporter} from './reporter';
   providers: [
     FileSystemService,
     FileSystemReporter
+  ],
+  dependencies: [
+    IsimudPersistence
   ]
 })
 export class IsimudFS {}
