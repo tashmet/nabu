efa997dba104f83b8eb113840ca31314e3832671
Björn Andersson <bjorn.olof.andersson@gmail.com>
2019-04-07 17:24:13 +0200

    Updated dependencies

--- src/sources/directory.ts
+++ src/sources/directory.ts
@@ -1,18 +1,17 @@
 import {ModelAnnotation} from '@ziggurat/amelatu';
-import {Newable} from '@ziggurat/meta';
 import {Container} from '@ziggurat/tiamat';
-import {Collection, CollectionConfig, SourceProducer} from '@ziggurat/isimud';
+import {Collection, CollectionConfig, Document, SourceProducer} from '@ziggurat/isimud';
 import {Persistence} from '@ziggurat/isimud-persistence';
 import {DirectoryConfig, FileSystemConfig} from '../interfaces';
 import {Directory} from '../adapters/directory';
 import {FSWatcher} from 'chokidar';
 
 export function directory(config: DirectoryConfig): SourceProducer {
-  return (container: Container, ctrlConfig: CollectionConfig, model: Newable<any>): Collection => {
+  return (container: Container, ctrlConfig: CollectionConfig): Collection => {
     const persistence = container.get<Persistence>('isimud.Persistence');
     const fsConfig = container.get<FileSystemConfig>('isimud.FileSystemConfig');
     const watcher = fsConfig.watch ? container.get<FSWatcher>('chokidar.FSWatcher') : undefined;
-    const modelName = ModelAnnotation.onClass(model)[0].name;
+    const modelName = ModelAnnotation.onClass(ctrlConfig.model || Document)[0].name;
     return persistence.createCollection(ctrlConfig, modelName,
       new Directory(config.serializer(container), config.path, config.extension, watcher)
     );
--- src/sources/file.ts
+++ src/sources/file.ts
@@ -1,18 +1,17 @@
 import {ModelAnnotation} from '@ziggurat/amelatu';
-import {Newable} from '@ziggurat/meta';
 import {Container} from '@ziggurat/tiamat';
-import {Collection, CollectionConfig, SourceProducer} from '@ziggurat/isimud';
+import {Collection, CollectionConfig, Document, SourceProducer} from '@ziggurat/isimud';
 import {Persistence} from '@ziggurat/isimud-persistence';
 import {FileConfig, FileSystemConfig} from '../interfaces';
 import {File} from '../adapters/file';
 import {FSWatcher} from 'chokidar';
 
 export function file(config: FileConfig): SourceProducer {
-  return (container: Container, ctrlConfig: CollectionConfig, model: Newable<any>): Collection => {
+  return (container: Container, ctrlConfig: CollectionConfig): Collection => {
     const persistence = container.get<Persistence>('isimud.Persistence');
     const fsConfig = container.get<FileSystemConfig>('isimud.FileSystemConfig');
     const watcher = fsConfig.watch ? container.get<FSWatcher>('chokidar.FSWatcher') : undefined;
-    const modelName = ModelAnnotation.onClass(model)[0].name;
+    const modelName = ModelAnnotation.onClass(ctrlConfig.model || Document)[0].name;
     return persistence.createCollection(ctrlConfig, modelName,
       new File(config.serializer(container), config.path, watcher)
     );
