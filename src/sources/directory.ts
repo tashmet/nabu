8686d173d2639d5960f748615da0f595c08e4b6f
Björn Andersson <bjorn.olof.andersson@gmail.com>
2019-04-23 16:02:54 +0200

    Removed dependency on @ziggurat/amelatu

--- src/sources/directory.ts
+++ src/sources/directory.ts
@@ -1,6 +1,6 @@
-import {ModelAnnotation} from '@ziggurat/amelatu';
+import {ModelAnnotation} from '@ziggurat/common';
 import {Container} from '@ziggurat/tiamat';
-import {Collection, CollectionConfig, Document, SourceProducer} from '@ziggurat/isimud';
+import {Collection, CollectionConfig, SourceProducer} from '@ziggurat/isimud';
 import {Persistence} from '@ziggurat/isimud-persistence';
 import {DirectoryConfig, FileSystemConfig} from '../interfaces';
 import {Directory} from '../adapters/directory';
@@ -11,9 +11,12 @@ export function directory(config: DirectoryConfig): SourceProducer {
     const persistence = container.get<Persistence>('isimud.Persistence');
     const fsConfig = container.get<FileSystemConfig>('isimud.FileSystemConfig');
     const watcher = fsConfig.watch ? container.get<FSWatcher>('chokidar.FSWatcher') : undefined;
-    const modelName = ModelAnnotation.onClass(ctrlConfig.model || Document)[0].name;
-    return persistence.createCollection(ctrlConfig, modelName,
-      new Directory(config.serializer(container), config.path, config.extension, watcher)
+    const modelName = ctrlConfig.model
+      ? ModelAnnotation.onClass(ctrlConfig.model || Document)[0].name
+      : undefined;
+    return persistence.createCollection(ctrlConfig,
+      new Directory(config.serializer(container), config.path, config.extension, watcher),
+      modelName
     );
   };
 }
--- src/sources/file.ts
+++ src/sources/file.ts
@@ -1,6 +1,6 @@
-import {ModelAnnotation} from '@ziggurat/amelatu';
+import {ModelAnnotation} from '@ziggurat/common';
 import {Container} from '@ziggurat/tiamat';
-import {Collection, CollectionConfig, Document, SourceProducer} from '@ziggurat/isimud';
+import {Collection, CollectionConfig, SourceProducer} from '@ziggurat/isimud';
 import {Persistence} from '@ziggurat/isimud-persistence';
 import {FileConfig, FileSystemConfig} from '../interfaces';
 import {File} from '../adapters/file';
@@ -11,9 +11,11 @@ export function file(config: FileConfig): SourceProducer {
     const persistence = container.get<Persistence>('isimud.Persistence');
     const fsConfig = container.get<FileSystemConfig>('isimud.FileSystemConfig');
     const watcher = fsConfig.watch ? container.get<FSWatcher>('chokidar.FSWatcher') : undefined;
-    const modelName = ModelAnnotation.onClass(ctrlConfig.model || Document)[0].name;
-    return persistence.createCollection(ctrlConfig, modelName,
-      new File(config.serializer(container), config.path, watcher)
+    const modelName = ctrlConfig.model
+      ? ModelAnnotation.onClass(ctrlConfig.model || Document)[0].name
+      : undefined;
+    return persistence.createCollection(ctrlConfig,
+      new File(config.serializer(container), config.path, watcher), modelName
     );
   };
 }
