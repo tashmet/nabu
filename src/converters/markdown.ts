bcd82305a6c37871714d2591a78fa534ab7e4e36
Björn Andersson <bjorn.olof.andersson@gmail.com>
2019-10-12 19:14:58 +0200

    Take the showdown converter as argument to markdown

--- src/converters/markdown.ts
+++ src/converters/markdown.ts
@@ -1,16 +1,12 @@
 import * as showdown from 'showdown';
 import {Converter} from '../interfaces';
 
-export function markdown(options?: showdown.ConverterOptions): Converter {
-  return new MarkdownConverter(options);
+export function markdown(converter: showdown.Converter): Converter {
+  return new MarkdownConverter(converter);
 }
 
 class MarkdownConverter {
-  private converter: showdown.Converter;
-
-  public constructor(options?: showdown.ConverterOptions) {
-    this.converter = new showdown.Converter(options);
-  }
+  public constructor(private converter: showdown.Converter) {}
 
   public async publish(text: string): Promise<string> {
     return this.converter.makeHtml(text);
